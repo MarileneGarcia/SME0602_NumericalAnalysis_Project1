ce8b5fc4630bc340ce7d50a3e57d9d9ae470ad66
Leandro A. Silva <leandroasilva290@gmail.com>
2020-06-01 13:01:56 -0300

    Ultimas alteracoes

--- ordem_convergencia.py
+++ ordem_convergencia.py
@@ -18,8 +18,8 @@ def ordem_convergencia( x, x_right ):
         if(d != 0):
             p =  n / d
 
-    c = e[tam-1] / pow(e[tam-2],p)
+    #c = e[tam-1] / pow(e[tam-2],p)
 
-    return c
+    return p
         
 
